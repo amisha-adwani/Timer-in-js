f3b63887acd007232607524878782e316d1a5798
amisha <amishaadwani0023@gmail.com>
2023-07-01 14:08:42 +0530

    stopwatch updated

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//------------------------------CLOCK------------------
+
 let output = document.getElementById("showTime");
 let button = (document.getElementById("Button").onclick = function () {
   const today = new Date();
@@ -10,23 +12,44 @@ let button = (document.getElementById("Button").onclick = function () {
 
 //------------------------------STOPWATCH------------------
 
-let Interval; 
-let h,m,s=0
+let Interval;
+let h,
+  m,
+  s = 0;
 
 let hours = document.getElementById("hours");
 let mins = document.getElementById("tens");
 let secs = document.getElementById("secs");
 
 document.getElementById("Start").onclick = function () {
- Interval= setInterval(timeSec, 1000);
-  function timeSec() {
-  secs.innerHTML++  
+  Interval = setInterval(time, 1000);
+  function time() {
+    secs.innerHTML++;
+    if (secs.innerHTML < 10) {
+      secs.innerHTML = "0" + secs.innerHTML;
+    }
+
+    if (secs.innerHTML === "60") {
+      secs.innerHTML = "00";
+      mins.innerHTML++;
+      if (mins.innerHTML < 10) {
+        mins.innerHTML = "0" + mins.innerHTML;
+      }
+    } 
+     else if (mins.innerHTML === "60") {
+      mins.innerHTML = "00";
+      hours.innerHTML++;
+      if (hours.innerHTML < 10) {
+        hours.innerHTML = "0" + hours.innerHTML;
+      }
+    }
   }
 };
-document.getElementById('Stop').onclick = function(){
-  clearInterval(Interval) 
-}
-document.getElementById('Reset').onclick=function(){
-  clearInterval(Interval) 
-  secs.innerHTML = '00'
-}
+
+document.getElementById("Stop").onclick = function () {
+  clearInterval(Interval);
+};
+document.getElementById("Reset").onclick = function () {
+  clearInterval(Interval);
+  secs.innerHTML = "00";
+};
